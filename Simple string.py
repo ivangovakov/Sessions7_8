b7497e19bd6c20c586824f7f21cb7437d533f80a
Ivan Govakov <govakivan@gmail.com>
2025-01-29 17:10:17 +0100

    1

---
+++ Simple string.py
@@ -0,0 +1,14 @@
+s1= 'banana'
+s2= "banana"
+print(s1, s2)
+s3= 'John said: "I\'m smart"'
+print (s3)
+s= "0123456789"
+print(s[1], s[7], s[9])
+print(s[-1], s[-4])
+print(f"the length of the string s is: {len(s)}")
+s1="hello"
+s2="world"
+print(s1+" "+s2)
+s4=4//2*(s1+" ")
+print(s4)
